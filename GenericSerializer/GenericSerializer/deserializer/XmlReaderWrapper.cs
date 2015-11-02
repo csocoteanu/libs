c6fc33b2692aee39c71e9cc470355f60977baba4
csocoteanu <cezar.socoteanu@gmail.com>
2015-11-02 19:04:15 +0200

    Initial stuff for the XmlReader implementation required for the deserializer

--- GenericSerializer/GenericSerializer/deserializer/XmlReaderWrapper.cs
+++ GenericSerializer/GenericSerializer/deserializer/XmlReaderWrapper.cs
@@ -3,23 +3,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace GenericSerializer.Deserializer
 {
     internal class XmlReaderWrapper : IXmlReader
     {
         private string m_inputPath;
+        private XmlReader m_reader;
+
+        public delegate void OnXmlNodeParsed(object sender, XmlParsedEventArgs args);
+        public OnXmlNodeParsed OnNodeParsed;
 
         public XmlReaderWrapper(string inputPath)
         {
             this.m_inputPath = inputPath;
+            m_reader = XmlReader.Create(inputPath);
+        }
+
+        public void ReadFully()
+        {
+            while (m_reader.Read())
+            {
+                switch (m_reader.NodeType)
+                {
+                    case XmlNodeType.Attribute:
+                        break;
+                    case XmlNodeType.Element:
+                        break;
+                    case XmlNodeType.EndElement:
+                        if (this.OnNodeParsed != null)
+                            this.OnNodeParsed(this, new XmlParsedEventArgs(eXmlItem.kNode, null));
+                        break;
+                    case XmlNodeType.XmlDeclaration:
+                    case XmlNodeType.ProcessingInstruction:
+                        break;
+                    case XmlNodeType.Text:
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         #region IDisposable
         public void Dispose()
         {
-
-        } 
+            if (this.m_reader != null)
+            {
+                this.m_reader.Close();
+            }
+        }
         #endregion
     }
 }
