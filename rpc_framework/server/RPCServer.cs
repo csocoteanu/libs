d366b5ed88e6bb1b14ff9cfc02edd6a3343e8c7a
csocoteanu <cezar.socoteanu@gmail.com>
2015-11-23 17:24:04 +0200

    RPC Framework: initial skeleton
    1. Add skeleton for the server
    2. Add skeleton for the ThreadPool

---
+++ rpc_framework/server/RPCServer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class RPCServer
+    {
+        private bool m_isRunning = false;
+        private string m_endPoint = null;
+        private ushort m_port = 0;
+        private ushort m_maxConnections = 0;
+
+        private Socket m_connectionSock = null;
+        private Thread m_serverThread = null;
+
+        public RPCServer()
+        {
+            this.m_endPoint = server.Properties.Settings.Default.kEndPoint;
+            this.m_port = server.Properties.Settings.Default.kPort;
+            this.m_maxConnections = server.Properties.Settings.Default.kMaxConnections;
+
+            this.m_connectionSock = this.InitConnectionSock();
+        }
+
+        private Socket InitConnectionSock()
+        {
+            IPAddress ipAddress = null;
+            IPEndPoint configuredEndPoint = null;
+            Socket sock = null;
+
+            if (IPAddress.TryParse(this.m_endPoint, out ipAddress))
+            {
+                // Establish the local endpoint for the socket.
+                configuredEndPoint = new IPEndPoint(ipAddress, this.m_port);
+
+                // Create a TCP/IP socket.
+                sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Bind the socket to the local endpoint and listen for incoming connections.
+                sock.Bind(configuredEndPoint);
+                sock.Listen(this.m_maxConnections);
+            }
+
+            return sock;
+        }
+
+        public void Start()
+        {
+            this.m_isRunning = true;
+            this.m_serverThread = new Thread(new ThreadStart(() =>
+            {
+                while (this.m_isRunning)
+                {
+                    lock (this)
+                    {
+                        if (!this.m_isRunning)
+                            break;
+                    }
+
+                    Socket newConnection = this.m_connectionSock.Accept();
+                    
+                }
+            }));
+
+            this.m_serverThread.Start();
+        }
+
+        public void Stop()
+        {
+            lock (this)
+            {
+                this.m_isRunning = false;
+            }
+
+            if (this.m_serverThread.IsAlive)
+                this.m_serverThread.Join();
+        }
+    }
+}
---
+++ rpc_framework/server/ThreadPool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class ThreadPool : IDisposable
+    {
+        public static readonly ThreadPool Instance = new ThreadPool();
+
+        private ushort m_threadPerCore = 0;
+        private int m_threadCount = 0;
+
+        private Thread[] m_workerThreads = null;
+        private Queue m_tasks = null;
+
+        private ThreadPool()
+        {
+            this.m_threadPerCore = server.Properties.Settings.Default.kThreadsPerCore;
+            this.m_threadCount = Environment.ProcessorCount * this.m_threadPerCore;
+
+            this.m_tasks = new Queue();
+
+            this.m_workerThreads = new Thread[this.m_threadCount];
+            for (int i = 0; i < this.m_workerThreads.Length; i++)
+            {
+                this.m_workerThreads[i] = new Thread(new ThreadStart(this.DoWork));
+                this.m_workerThreads[i].Start();
+            }
+        }
+
+        private void DoWork()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < this.m_workerThreads.Length; i++)
+            {
+                this.m_workerThreads[i].Join();
+            }
+        }
+    }
+}
